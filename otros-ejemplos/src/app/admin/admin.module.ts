37f9eeff32ba9332e5703e3829ecc998b8749880
AngelVil92 <angelvil92@gmail.com>
2021-05-06 14:05:13 +0200

    Día 1: preparando el lazy loading

---
+++ otros-ejemplos/src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PanelAdminComponent } from './panel-admin/panel-admin.component';
+
+
+
+@NgModule({
+  declarations: [
+    PanelAdminComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
